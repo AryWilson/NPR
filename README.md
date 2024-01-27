3f4a08d1a594c186212ae80beb3ef903c15cb732
AryWilson <awilson2@brynmawr.edu>
2024-01-27 16:12:19 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # NPR
-Filters for use in real-time, non-photorealistic image processing.
+
+A collection of filters useful in non-photorealistic post-processing
+
+## How to build
+
+*Windows*
+
+Open git bash to the directory containing this repository.
+
+```
+project-template $ mkdir build
+project-template $ cd build
+project-template/build $ cmake ..
+project-template/build $ start NPR.sln
+```
+
+To run from the git bash command shell, 
+
+```
+project-template/build $ ../bin/Debug/demo.exe
+```
+
+*macOS and unix*
+
+Open terminal to the directory containing this repository.
+
+```
+project-template $ mkdir build
+project-template $ cd build
+project-template/build $ cmake ..
+project-template/build $ make
+```
+
+To run each program from build, you would type
+
+```
+project-template/build $ ../bin/pixmap_art
+```
+
+
+## Examples
+
---
+++ src/image.h
@@ -0,0 +1,231 @@
+// Copyright 2021, Aline Normoyle, alinen
+
+#ifndef AGL_IMAGE_H_
+#define AGL_IMAGE_H_
+
+#include <iostream>
+#include <string>
+
+namespace agl {
+
+/**
+ * @brief Holder for a RGB color
+ * 
+ */
+struct Pixel {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
+
+/**
+ * @brief Implements loading, modifying, and saving RGB images
+ */
+class Image {
+ public:
+  Image();
+  Image(int width, int height);
+  Image(const Image& orig);
+  Image& operator=(const Image& orig);
+
+
+  virtual ~Image();
+  void clean();
+
+  /** 
+   * @brief Load the given filename 
+   * @param filename The file to load, relative to the running directory
+   * @param flip Whether the file should flipped vertally when loaded
+   * 
+   * @verbinclude sprites.cpp
+   */
+  bool load(const std::string& filename, bool flip = false);
+
+  /** 
+   * @brief Save the image to the given filename (.png)
+   * @param filename The file to load, relative to the running directory
+   * @param flip Whether the file should flipped vertally before being saved
+   */
+  bool save(const std::string& filename, bool flip = true) const;
+
+  /** @brief Return the image width in pixels
+   */
+  int width() const;
+
+  /** @brief Return the image height in pixels
+   */
+  int height() const;
+
+  /** 
+   * @brief Return the RGB data
+   *
+   * Data will have size width * height * 4 (RGB)
+   */
+  char* data() const;
+
+  /**
+   * @brief Replace image RGB data
+   * @param width The new image width
+   * @param height The new image height
+   *
+   * This call will replace the old data with the new data. Data should 
+   * match the size width * height * 3
+   */
+  void set(int width, int height, unsigned char* data);
+
+  /**
+   * @brief Get the pixel at index (row, col)
+   * @param row The row (value between 0 and height)
+   * @param col The col (value between 0 and width)
+   *
+   * Pixel colors are unsigned char, e.g. in range 0 to 255
+   */ 
+  Pixel get(int row, int col) const;
+
+  /**
+   * @brief Set the pixel RGBA color at index (row, col)
+   * @param row The row (value between 0 and height)
+   * @param col The col (value between 0 and width)
+   *
+   * Pixel colors are unsigned char, e.g. in range 0 to 255
+   */ 
+  void set(int row, int col, const Pixel& color);
+
+  /**
+ * @brief Set the pixel RGB color at index i
+ * @param i The index (value between 0 and width * height)
+ *
+ * Pixel colors are unsigned char, e.g. in range 0 to 255
+ */
+  Pixel get(int i) const;
+
+  /**
+ * @brief Set the pixel RGB color at index i
+ * @param i The index (value between 0 and width * height)
+ *
+ * Pixel colors are unsigned char, e.g. in range 0 to 255
+ */
+  void set(int i, const Pixel& c);
+
+
+  // resize the image
+  Image resize(int width, int height) const;
+
+  // flip around the horizontal midline
+  Image flipHorizontal() const;
+
+  // flip around the vertical midline
+  Image flipVertical() const;
+
+  // rotate the Image 90 degrees
+  Image rotate90() const;
+
+  // Return a sub-Image having the given top,left coordinate and (width, height)
+  Image subimage(int x, int y, int w, int h) const;
+
+  // Replace the portion starting at (row, col) with the given image
+  // Clamps the image if it doesn't fit on this image
+  void replace(const Image& image, int x, int y);
+
+  // swirl the colors 
+  Image swirl() const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = this.pixel + other.pixel
+  // Assumes that the two images are the same size
+  Image add(const Image& other) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = this.pixel - other.pixel
+  // Assumes that the two images are the same size
+  Image subtract(const Image& other) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = this.pixel * other.pixel
+  // Assumes that the two images are the same size
+  Image multiply(const Image& other) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = abs(this.pixel - other.pixel)
+  // Assumes that the two images are the same size
+  Image difference(const Image& other) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = max(this.pixel, other.pixel)
+  // Assumes that the two images are the same size
+  Image lightest(const Image& other) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    result.pixel = min(this.pixel, other.pixel)
+  // Assumes that the two images are the same size
+  Image darkest(const Image& other) const;
+
+  // only display the specified rgb color of the image
+  // default returns grayscale
+  Image monochrome(int opt) const;
+
+  // makes the image more vibrant by the specified ammount
+  Image saturate(unsigned char ammount) const;
+  // makes the darkest values darker and the lightest values lighter
+  Image contrast(unsigned char ammount) const;
+
+  // applies red-teal filter
+  Image redTeal(unsigned char ammount) const;
+
+  // returns the average va;ue [0,255] of a given pixel
+  // optional greyscale flag
+  // unsigned char average(struct Pixel rgb, bool greyscale = false);
+
+  // Sobel Edge Detector
+  Image edgeFinder()const;  
+
+  // box blur
+  Image blur() const;
+
+
+
+  // Apply gamma correction
+  Image gammaCorrect(float gamma) const;
+
+  // Apply the following calculation to the pixels in 
+  // our image and the given image:
+  //    this.pixels = this.pixels * (1-alpha) + other.pixel * alpha
+  // Assumes that the two images are the same size
+  Image alphaBlend(const Image& other, float amount) const;
+
+  // Convert the image to grayscale
+  Image invert() const;
+
+  // Convert the image to grayscale
+  Image grayscale() const;
+
+  // return a bitmap version of this image
+  Image colorJitter(int size) const;
+
+  // return a bitmap version of this image
+  Image bitmap(int size) const;
+
+  // Fill this image with a color
+  void fill(const Pixel& c);
+
+    // very rough color quantization
+  Image cquant() const;
+
+  // kernel color randomization
+  Image crand() const;
+
+ private:
+  int w,h,ch = 0;
+  struct Pixel * _data = nullptr;
+  bool stb_load = false;
+
+};
+}  // namespace agl
+#endif  // AGL_IMAGE_H_
